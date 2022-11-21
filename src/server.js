b7ee31bc023cd4dd051f1573fb4962c4036e0666
Jarsters <kepalabotak9876@gmail.com>
2022-11-21 14:15:55 +0700

    memperbaiki host

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ BigInt.prototype.toJSON = function () {
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 5000,
-    host: process.env.HOST || 'localhost',
+    host: process.env.HOST || '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
