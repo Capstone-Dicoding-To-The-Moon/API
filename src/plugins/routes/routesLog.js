570786dc32e9251079b30109f5ba5c69c6275be3
Jarsters <kepalabotak9876@gmail.com>
2022-11-21 15:37:32 +0700

    Untuk repo Full-Family-Preparation

--- src/plugins/routes/routesLog.js
+++
@@ -1,5 +0,0 @@
-const { getAllLogOrderByIdDescending } = require('../handlers/logHandlers');
-const { routesHelper } = require('../helpers/routesHelper');
-
-const routesLog = [routesHelper('GET', '/log', getAllLogOrderByIdDescending)];
-module.exports = routesLog;
